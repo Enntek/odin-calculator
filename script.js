96779255d48f4be815dfcd7678f3eef40b87d3fb
Enntek <abluecar@gmail.com>
2022-07-13 13:53:39 -0400

    Add keyboard eventListeners, finish most logic
    Last bit of logic to work out: multiple operations
    i.e. 4 + 2 x 3 - 15

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ calcTitle.setAttribute('class', 'calcTitle')
 calcTitle.classList.add('content');
 calcTitle.textContent = "Odin Calculator"
 
-
 const calcDisplay = document.createElement('div') // text
 calcDisplay.setAttribute('class', 'calcDisplay')
 calcDisplay.classList.add('content');
@@ -23,7 +22,7 @@ calcDisplay.textContent = "'0'"
 const calcModel = document.createElement('div') //text
 calcModel.setAttribute('class', 'calcModel');
 calcModel.classList.add('content');
-calcModel.textContent = '10-Digit OC57418'
+calcModel.textContent = '10-Digit OC-57418'
 
 const calcSolarFrame = document.createElement('div') // calcSolarFrame
 calcSolarFrame.setAttribute('class', 'calcSolarFrame');
@@ -48,27 +47,24 @@ const calcBottom = document.createElement('div')
 calcBottom.setAttribute('class', 'calcBottom');
 calcFrame.appendChild(calcBottom);
 
-
 //create buttons
 for (i = 0; i < 20; i++) {
   const btn = document.createElement('button');
   btn.classList.add('btn', 'btnSmall');
   btn.setAttribute('id', `btn${i}`);
   btn.textContent = `${i}`;
-  btn.addEventListener('click', buttonPress)
-  
+  btn.addEventListener('click', keyPress)
   calcBottom.appendChild(btn)
 }
 
-
-const btnPlus = document.createElement('button'); //bigger button lays on top of 2 buttons
+//create bigger button separately for '+'
+const btnPlus = document.createElement('button');
 btnPlus.classList.add('content');
 btnPlus.classList.add('class', 'btn');
 btnPlus.setAttribute('id', 'btnPlus');
-btnPlus.addEventListener('click', buttonPress)
+btnPlus.addEventListener('click', keyPress)
 btnPlus.textContent = "+";
 calcBottom.append(btnPlus);
-
 
 document.getElementById("btn0").textContent = '7'
 document.getElementById("btn1").textContent = '8'
@@ -92,19 +88,6 @@ document.getElementById("btn17").textContent = '%'
 document.getElementById("btn18").textContent = 'na'
 document.getElementById("btn19").textContent = '='
 
-
-// allButtons.forEach(button => console.log(button.id))
-
-// document.querySelector('#1').textContent = '999';
-
-// calc logic
-// add eventlistener to each button
-// check if button textcontent is a number OR a decimal
-// add that to display
-
-// let display = calcDisplay.textContent;
-// display = '5555';
-
 calcDisplay.textContent = '';
 let previousString = '';
 let currentString = '';
@@ -124,139 +107,140 @@ function operateAB(a, b, sign) {
   }
 }
 
-function buttonPress(e) {
+function keyPress(e) {
 
-  let currentPress = e.target.textContent;
+  
+
+  function getKey(e) {
+
+    let isValidKeyStroke = (e) => {
+      if (/[0-9.+*\-\/]/.test(e.key) || 
+          (e.key == 'Enter') ||
+          (e.key == 'Backspace') ||
+          (e.key == 'Delete')
+          ) return true;
+    }
+  
+    if (e instanceof KeyboardEvent && isValidKeyStroke(e)) {
+      switch(e.key) {
+        case 'Enter':
+          return ('=');
+        case 'Backspace':
+          return ('CE');
+        case 'Delete':
+          return ('C');
+        case '/':
+          return ('÷');
+        case '*':
+          return ('x');
+        default:
+          return (e.key);
+      }
+
+    } else {
+      if (e.target.textContent) return e.target.textContent;
+    }
+  }
+
+  
+  let currentPress = getKey(e);
+
+  console.log(currentPress);
+
 
   let isNumber = (a) => (isFinite(a) && !isNaN(a));
 
   let isOperator = (a) => (/[+x÷-]/.test(a))
 
-  // if press C
-  // clear all memory, clear display
-  if (currentPress == 'C') {
+  let clearMemory = () => {
     calcDisplay.textContent = '';
     currentString = '';
-    previousString = '';
-    operator = '';
+    previousString = ''; 
+    operator = ''; 
     previousPress = '';
-  }
-  // if press CE
+  };
+
+  if (currentPress == 'C') {clearMemory()};
+  if (currentPress == 'CE' && previousPress == 'CE') {clearMemory()};
   
-  // if number check previous key press, 
-  // > if number OR decimal, add to string
-  if (isNumber(currentPress) && !isOperator(previousPress)) {
-    previousPress = currentPress;
-    currentString += currentPress;
-    calcDisplay.textContent = currentString;
-  } else if (isNumber(currentPress) && isOperator(previousPress)) {
-    previousString = currentString;
-    currentString = ''
-    currentString += currentPress;
-    calcDisplay.textContent = currentString;
+
+  if (currentPress == 'CE') {
+    previousPress = 'CE';
+    currentString = '';
+    calcDisplay.textContent = '';
+  }
+  
+  if (isNumber(currentPress) && calcDisplay.textContent !== 'E'){
+
+    if (+calcDisplay.textContent == 0) {currentString = ''}; //remove leading 0
+
+    if (!isOperator(previousPress) && previousPress == '=') {
+    
+      previousPress = '';
+      previousString = '';
+      operator = '';
+      previousPress = currentPress;
+      currentString = ''
+      currentString += currentPress
+      calcDisplay.textContent = currentString;
+  
+    } else if (!isOperator(previousPress)) {
+      
+      previousPress = currentPress;
+      currentString += currentPress;
+      calcDisplay.textContent = currentString;
+  
+    } 
+
+    else if (isOperator(previousPress)) {
+
+      previousPress = currentPress;
+      previousString = currentString;
+      currentString = ''
+      currentString += currentPress;
+      calcDisplay.textContent = currentString;
+
+    }
   }
 
-  // if press decimal check if already decimal
-  
   if (currentPress == '.' && !currentString.includes('.')) {
-    // previous press??
     currentString += '.'
     calcDisplay.textContent = currentString;
   }
 
-  // if press operator, check last keypress
-  // > if previousPress was operator, replace operator AND previousPress
-  // > if previousPress was number, store number in previousString, store operator, clear currentString
-  if (isOperator(currentPress)) {
+  if (isOperator(currentPress) && previousPress!== '=') {
+    operator = currentPress;
+    previousPress = currentPress;
+  } else if (isOperator(currentPress) && previousPress == '=') {
     operator = currentPress;
     previousPress = currentPress;
   }
   
-  // if press =
   if (currentPress == '=') {
+
+    if(operator == '÷' && +currentString == 0) return calcDisplay.textContent='Oh no!';
+
     calcDisplay.textContent = operateAB(previousString, currentString, operator);
+    previousPress = '=';
+    previousString = calcDisplay.textContent;
   };
 
-  // if press %
+  if (currentPress == '%' && isNumber(calcDisplay.textContent)) {
+    calcDisplay.textContent = calcDisplay.textContent * .01
+    currentString = calcDisplay.textContent;
+  }
+
+  if (calcDisplay.textContent % 1 != 0 && calcDisplay.textContent !== 'E') {
+    calcDisplay.textContent = Math.round(calcDisplay.textContent*100000)/100000
+  };
+
+  if (calcDisplay.textContent.length > 10) {
+    clearMemory();
+    calcDisplay.textContent ='E';
+  }
+
 }
 
-
-// function buttonPressOld(e) {
-
-//   let currentPress = e.target.textContent;
-
-//   // press CE twice or C once 
-//   if ((e.target.textContent == 'CE' && previousPress == 'CE') || (e.target.textContent == 'C')) {
-//     calcDisplay.textContent = '0';
-//     previousString = '';
-//     currentString = '';
-//     operator = '';
-//   };
-
-//   // press CE once
-//   if (e.target.textContent == 'CE') {
-//     calcDisplay.textContent = '0';
-//     previousPress = 'CE';
-//     operator = '';
-//   };
-  
-//   // press 0-9
-//   if (!isNaN(currentPress) && isFinite(currentPress) && e.target.textContent !== '.') {
-
-//     if (calcDisplay.textContent == '0') {calcDisplay.textContent = ''}; // remove left 0 before other nums
-    
-//     if ((previousString !== '') && (operator !== '') && (currentString == '')) {    //clear display after pressing operator
-//       calcDisplay.textContent = '';
-//     }
-
-//     calcDisplay.textContent += currentPress;
-//     currentString = calcDisplay.textContent;
-//   }
-
-//   // press .
-//   if (e.target.textContent == '.' && !calcDisplay.textContent.includes('.')) {  // avoid multiple .
-//     calcDisplay.textContent += ".";
-//     previousString = currentString;
-//   }
-  
-//   // press + - * / 
-//   if (/[+x÷-]/.test(e.target.textContent)) {          
-//     previousString = calcDisplay.textContent;
-//     currentString = '';
-    
-//     if (e.target.textContent == 'x') {
-//       operator = "*";
-//     } else if (e.target.textContent == '÷') {
-//       operator = "/"
-//     } else {
-//       operator = e.target.textContent;
-//     }
-
-//   };
-
-//   //press =
-//   if (e.target.textContent == '=' && currentString !== '') {
-//     currentString = calcDisplay.textContent;
-//     calcDisplay.textContent = +previousString + +currentString;
-
-//     previousString = calcDisplay.textContent;
-//     currentString = '';
-
-//     // calcDisplay.textContent = previousString + operator + currentString;
-
-//   }
-
-// }
-
-  // console.log(e.target.id);
-  // console.log(e.target.className);
-
-// center text on buttons, mobile looks off
-// remove dot, then add it back at the end
-// add hover effect, make it half-subtle
+window.addEventListener('keydown', keyPress)
 // use button-loop to add big button, do not do it separately
-
-// when using global variables, you can change its value by just reassigning it,
-// DO NOT USE LET, unless you want to keep its new value + scope in the function
-// i.e. var2 = 3.3 WILL REASSIGN GLOBAL VARIABLE
-// i.e. let var3 = 200 WILL NOT REASSIGN GLOBAL 
+// work out logic to multiple operations, i.e. 5 + 2 * 3 - 1
